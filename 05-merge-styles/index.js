859ebba1a5ef0c8a0046a11e02882acffc75ec03
Legat <zia-d@mail.ru>
2022-05-22 14:44:52 +0400

    feat: solve fifth task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const eventEmitter = require('events');
+
+const distFolderPath = path.join(__dirname, 'project-dist');
+const bundleFilePath = path.join(distFolderPath, 'bundle.css');
+const stylesFolderPath = path.join(__dirname, 'styles');
+const emitter = new eventEmitter();
+let stylesFolderContent = [];
+const output = fs.createWriteStream(bundleFilePath);
+
+fs.readdir(stylesFolderPath, (error, data) => {
+  if (error) console.log(error);
+  else stylesFolderContent = data;
+  stylesFolderContent.forEach(file => {
+    const filePath = path.join(stylesFolderPath, file);
+    const fileExt = file.slice(-4);
+    fs.stat(filePath, (error, stats) => {
+      if (error) console.log(error);
+      if (fileExt === '.css' && stats.isFile()) {
+        const readableStream = fs.createReadStream(filePath, 'utf-8');
+        readableStream.on('data', chunk => {
+          output.write(chunk);
+        });
+        readableStream.on('end', () => {
+        });
+      }
+    });
+  });
+  emitter.emit('filesListReady');
+});
